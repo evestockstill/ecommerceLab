9e743cb785ae5d616cae04319188edcb8b375740
Eve Stockstill <evestockstillpdx@gmail.com>
2019-10-07 22:10:54 -0700

    products page

--- src/renderProducts.js
+++ src/renderProducts.js
@@ -1,28 +1,23 @@
-function renderFruit(fruit) {
+function renderProduct(cues) {
     const li = document.createElement('li');
-    li.className = fruit.category;
-    li.title = fruit.description;
+    li.className = cues.category;
+    li.title = cues.description;
 
     const h3 = document.createElement('h3');
-    h3.textContent = fruit.name;
+    h3.textContent = cues.name;
     li.appendChild(h3);
 
     const img = document.createElement('img');
-    img.src = fruit.image;
-    img.alt = fruit.name + ' image';
+    img.src = cues.image;
+    img.alt = cues.name + ' image';
     li.appendChild(img);
 
     const p = document.createElement('p');
     p.className = 'price';
 
-    const usd = fruit.price.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
-    // p.textContent = usd;
-    const priceTextNode = document.createTextNode(usd);
-    p.appendChild(priceTextNode);
-
     const button = document.createElement('button');
     button.textContent = 'Add';
-    button.value = fruit.code;
+    button.value = cues.id;
     p.appendChild(button);
 
     li.appendChild(p);
@@ -30,4 +25,6 @@ function renderFruit(fruit) {
     return li;
 }
 
-export default renderFruit;
+export default renderProducts;
+
+
