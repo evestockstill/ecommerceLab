12d063d34c092e900556aff1538d2e25df9d1f11
Eve Stockstill <evestockstillpdx@gmail.com>
2019-10-08 20:24:23 -0700

    passing test products

--- test/renderProducts.test.js
+++ test/renderProducts.test.js
@@ -15,7 +15,7 @@ test('renders a product', (assert) => {
         category: 'playing-cue',
         price: 1500,
     };
-    const expected = '<li class="playing-cue" title="Limited Edition Ebony Cue with Simulated Ivory"><h3>J.Pechauer Cues Limited Edition 50th Anniversary</h3><img src="../assets/peachauer1400.jpg" alt="J.Pechauer Cues Limited Edition 50th Anniversary image"><p class="price">$1,500.00$1,500.00<button value="peachauer">Add</button></p></li>';
+    const expected = '<li class="playing-cue" title="Limited Edition Ebony Cue with Simulated Ivory"><h3>J.Pechauer Cues Limited Edition 50th Anniversary</h3><img src="../assets/peachauer1400.jpg" alt="J.Pechauer Cues Limited Edition 50th Anniversary image"><p class="price">$1,500.00<button value="peachauer">Add</button></p></li>';
     const dom = renderProduct(peachauer);
     const html = dom.outerHTML;
 
