f4aee7a5b2706709cfa5385c09dc40b7b21c3fef
Eve Stockstill <evestockstillpdx@gmail.com>
2019-10-12 20:45:53 -0700

    home page

--- shopping-cart/app.js
+++ shopping-cart/app.js
@@ -3,6 +3,21 @@ import cues from '../data/cues.js';
 import { cart } from '../data/cart.js';
 import { toUSD } from '../common/utils.js';
 
+// const pageLinks = document.querySelector('page_links');
+// const navBtn = document.getElementById('button');
+// navBtn.addEventListener('click', function() {
+//     let rotate = pageLinks.classList.contains('hide');
+//     if (rotate) {
+//         pageLinks.classList.remove('hide');
+//         navBtn.classList.remove('unhide');
+//     }
+//     else {
+//         pageLinks.classList.add('hide');
+//         navBtn.classList.add('unhide');
+//     }
+// });
+
+
 const tableElement = document.querySelector('tbody');
 
 let cartTotal = 0;
@@ -19,4 +34,3 @@ cart.forEach(cueOrder => {
     const totalCell = document.getElementById('order-total-cell');
     totalCell.textContent = toUSD(cartTotal);
 });
-const navBtn = document.querySelector('nav-button');
